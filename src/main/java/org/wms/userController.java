86b0744ed61fa1a861e41b86167934083f090b36
sachinvarshney <sachinvarshney459@gmail.com>
2024-02-19 19:31:07 +0530

    added order summary for bedSofaTable

--- src/main/java/org/wms/userController.java
+++ src/main/java/org/wms/userController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -53,13 +54,37 @@ public class userController implements Initializable {
         DatabaseConnection connection = new DatabaseConnection();
         connectionDB = connection.getConnection();
     }
-
+    @FXML
+    private AnchorPane summarySection;
     @FXML
     private ComboBox<String> bedQuality;
     @FXML
     private ComboBox<String> sofaQuality;
     @FXML
     private ComboBox<String> tableQuality;
+    @FXML
+    private AnchorPane orderSummaryBed;
+    @FXML
+    private AnchorPane orderSummarySofa;
+    @FXML
+    private AnchorPane orderSummaryTable;
+    @FXML
+    private AnchorPane orderSummarySofaBed;
+    @FXML
+    private TextField userAddress;
+    @FXML
+    private TextField detail_quality;
+    @FXML
+    private TextField orderUserName;
+    @FXML
+    private AnchorPane orderSummaryBedTable;
+    @FXML
+    private AnchorPane orderSummarySofaTable;
+    @FXML
+    private AnchorPane orderSummarySofaBedTable;
+    @FXML
+    private AnchorPane orderDetail;
+
     @FXML
     private ComboBox<String> bedQuantity;
     @FXML
@@ -79,11 +104,40 @@ public class userController implements Initializable {
     @FXML
     private Button placeOrderBtn;
     @FXML
+    private TextField orderID_details;
+    @FXML
     private TextField nameField;
     @FXML
     private TextField address_order;
     @FXML
     private TextField feedback_order;
+    @FXML
+    private TextField bts_add;
+
+    @FXML
+    private TextField bts_b_ql;
+
+    @FXML
+    private TextField bts_b_qt;
+
+    @FXML
+    private TextField bts_id;
+
+    @FXML
+    private TextField bts_name;
+
+    @FXML
+    private TextField bts_s_ql;
+
+    @FXML
+    private TextField bts_s_qt;
+
+    @FXML
+    private TextField bts_t_ql;
+
+    @FXML
+    private TextField bts_t_qt;
+
 
 
     @Override
@@ -132,9 +186,10 @@ public class userController implements Initializable {
             logger.error("Error closing place order", ex);
         }
     }
+    String orderID = generateID();
 
     public void placeOrder() throws OrderPlacementException {
-        if(address_order.getText().isBlank() || feedback_order.getText().isBlank()){
+        if (address_order.getText().isBlank() || feedback_order.getText().isBlank()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setContentText("Please fill the Order details before proceeding");
@@ -145,47 +200,144 @@ public class userController implements Initializable {
             return;
         }
         MapQuality map = new MapQuality();
-        try {
             ArrayList<OrderItem> items = new ArrayList<>();
             if (bedQuality.getValue() != null && bedQuantity.getValue() != null) {
                 int bedQL = map.getQualitiesValues(bedQuality.getValue().toLowerCase());
                 int bedQN = Integer.parseInt(bedQuantity.getValue());
                 items.add(new OrderItem("bed", bedQL, bedQN));
-
             }
             if (sofaQuality.getValue() != null && sofaQuantity.getValue() != null) {
                 int sofaQL = map.getQualitiesValues(sofaQuality.getValue().toLowerCase());
                 int sofaQN = Integer.parseInt(sofaQuantity.getValue());
                 items.add(new OrderItem("sofa", sofaQL, sofaQN));
-
             }
             if (tableQuality.getValue() != null && tableQuantity.getValue() != null) {
                 int tableQN = Integer.parseInt(tableQuantity.getValue());
                 int tableQL = map.getQualitiesValues(tableQuality.getValue().toLowerCase());
                 items.add(new OrderItem("table", tableQL, tableQN));
             }
-            if(items.isEmpty()){
+            logger.info("Printing the Placed items ");
+
+            if (items.isEmpty()) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setContentText("Your Selection is Empty");
                 alert.showAndWait();
                 return;
             }
-            String orderID = generateID();
-            boolean res = insertIntoOrder(items, orderID);
-            if (!res) {
-                throw new OrderPlacementException("Failed to place the order.");
-            } else {
-                Stage stage = (Stage) placeOrderBtn.getScene().getWindow();
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("thanksView.fxml"));
-                Scene scene = new Scene(fxmlLoader.load());
-                stage.setScene(scene);
+
+            int sizeOfList = items.size();
+            if (sizeOfList == 1) {
+                summarySection.setVisible(true);
+                orderDetail.setVisible(false);
+                String cName = items.get(0).getCommodity();
+                if (cName.equals("bed")) {
+                    logger.info("Bed Only");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(true);
+                    orderSummarySofa.setVisible(false);
+                    orderSummaryTable.setVisible(false);
+                    orderSummarySofaBed.setVisible(false);
+                    orderSummaryBedTable.setVisible(false);
+                    orderSummarySofaTable.setVisible(false);
+                } else if (cName.equals("sofa")) {
+                    logger.info("Sofa only");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(false);
+                    orderSummarySofa.setVisible(true);
+                    orderSummaryTable.setVisible(false);
+                    orderSummarySofaBed.setVisible(false);
+                    orderSummaryBedTable.setVisible(false);
+                    orderSummarySofaTable.setVisible(false);
+                } else if (cName.equals("table")) {
+                    logger.info("table Only ");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(false);
+                    orderSummarySofa.setVisible(false);
+                    orderSummaryTable.setVisible(true);
+                    orderSummarySofaBed.setVisible(false);
+                    orderSummaryBedTable.setVisible(false);
+                    orderSummarySofaTable.setVisible(false);
+                }
+
+            } else if (sizeOfList == 2) {
+                summarySection.setVisible(true);
+                orderDetail.setVisible(false);
+                String cName1 = items.get(0).getCommodity();
+                String cName2 = items.get(1).getCommodity();
+                String Cname = cName1 + cName2;
+                if (Cname.equals("sofabed") || Cname.equals("bedsofa")) {
+                    logger.info("Sofa Bed Only ");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(false);
+                    orderSummarySofa.setVisible(false);
+                    orderSummaryTable.setVisible(false);
+                    orderSummarySofaBed.setVisible(true);
+                    orderSummaryBedTable.setVisible(false);
+                    orderSummarySofaTable.setVisible(false);
+                } else if (Cname.equals("bedtable") || Cname.equals("tablebed")) {
+                    logger.info("Bed and table ");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(false);
+                    orderSummarySofa.setVisible(false);
+                    orderSummaryTable.setVisible(false);
+                    orderSummarySofaBed.setVisible(false);
+                    orderSummaryBedTable.setVisible(true);
+                    orderSummarySofaTable.setVisible(false);
+                } else if (Cname.equals("sofatable") || Cname.equals("tablesofa")) {
+                    logger.info("Sofa and table only ");
+                    orderSummarySofaBedTable.setVisible(false);
+                    orderSummaryBed.setVisible(false);
+                    orderSummarySofa.setVisible(false);
+                    orderSummaryTable.setVisible(false);
+                    orderSummarySofaBed.setVisible(false);
+                    orderSummaryBedTable.setVisible(false);
+                    orderSummarySofaTable.setVisible(true);
+                }
+            } else if (sizeOfList == 3) {
+                summarySection.setVisible(true);
+                orderDetail.setVisible(false);
+                logger.info("Sofa bed and table ");
+                orderSummarySofaBedTable.setVisible(true);
+                orderSummaryBed.setVisible(false);
+                orderSummarySofa.setVisible(false);
+                orderSummaryTable.setVisible(false);
+                orderSummarySofaBed.setVisible(false);
+                orderSummaryBedTable.setVisible(false);
+                orderSummarySofaTable.setVisible(false);
+                bts_name.setText(userName);
+               // bts_add.setText(userAddress.getText());
+                bts_id.setText(orderID);
+                for (OrderItem item : items) {
+                    if (item.getCommodity().equals("bed")) {
+                        // Set the quality and quantity of the bed
+                        bts_b_ql.setText(Integer.toString(item.getQuality()));
+                        bts_b_qt.setText(Integer.toString(item.getQuantity()));
+                    } else if (item.getCommodity().equals("sofa")) {
+                        // Set the quality and quantity of the sofa
+                        bts_s_ql.setText(Integer.toString(item.getQuality()));
+                        bts_s_qt.setText(Integer.toString(item.getQuantity()));
+                    } else if (item.getCommodity().equals("table")) {
+                        // Set the quality and quantity of the table
+                        bts_t_ql.setText(Integer.toString(item.getQuality()));
+                        bts_t_qt.setText(Integer.toString(item.getQuantity()));
+                    }
+                }
+
+                // Place the order
+//                boolean res = insertIntoOrder(items, orderID);
+//                if (!res) {
+//                    throw new OrderPlacementException("Failed to place the order.");
+//                } else {
+//                    Stage stage = (Stage) placeOrderBtn.getScene().getWindow();
+//                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("thanksView.fxml"));
+//                    Scene scene = new Scene(fxmlLoader.load());
+//                    stage.setScene(scene);
+//                }
             }
-        } catch (IOException | SQLException ex) {
-            logger.error("Error placing order", ex);
-            throw new OrderPlacementException("Error placing order");
-        }
+
     }
+
 
     private boolean insertIntoOrder(ArrayList<OrderItem> items, String orderID) throws SQLException {
         int commodityID = -1;
